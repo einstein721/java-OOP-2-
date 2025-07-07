66e54dee3f02852d3fd60c81b649b97839258298
einstein721 <einstein.ombati@strathmore.edu>
2025-07-07 16:25:35 +0300

    Add files via upload

---
+++ AbstractClassesAndInterfaces/DataSource.java
@@ -0,0 +1,3 @@
+public interface DataSource {
+    void execute();
+}
---
+++ AbstractClassesAndInterfaces/MainApplication.java
@@ -0,0 +1,11 @@
+public class MainApplication {
+
+    public static void main(String[] args) {
+
+        Admin curAdmin = new Admin(1234, "Stephan", "xa1@LW42%3");
+        User curUser = new User(4455, "John");
+
+        curAdmin.performOperation(new Update()); // Admin updates the data
+        curUser.performOperation(new View());    // User views the data
+    }
+}
